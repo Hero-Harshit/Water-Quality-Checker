091bb028f71685ffa8910d9390a666b18de056d4
Hero-Harshit <heroharshitlaptop17@gmail.com>
2025-08-10 22:49:35 +0530

    Basic Wqc Site

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById("check_button").onclick = () => {
+
+    let temp_value = document.getElementById("temp_input").value;
+    let ph_value = document.getElementById("ph_input").value;
+    let tds_value = document.getElementById("tds_input").value;
+    let hardness_value = document.getElementById("hardness_input").value;
+    let turbidity_value = document.getElementById("turbidity_input").value;
+    let salanity_value = document.getElementById("salanity_input").value;
+    let orp_value = document.getElementById("orp_input").value;
+    let ec_value = document.getElementById("ec_input").value;
+    let do_value = document.getElementById("do_input").value;
+
+}
