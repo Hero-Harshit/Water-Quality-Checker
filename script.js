7c6acbce092aafc687811a71cbd45a31e73238e4
Hero-Harshit <heroharshitlaptop17@gmail.com>
2025-08-11 09:48:07 +0530

    Switched from p -> label & some other UI improvements.

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.getElementById("check_button").onclick = () => {
     let tds_value = document.getElementById("tds_input").value;
     let hardness_value = document.getElementById("hardness_input").value;
     let turbidity_value = document.getElementById("turbidity_input").value;
-    let salanity_value = document.getElementById("salanity_input").value;
+    let salanity_value = document.getElementById("salinity_input").value;
     let orp_value = document.getElementById("orp_input").value;
     let ec_value = document.getElementById("ec_input").value;
     let do_value = document.getElementById("do_input").value;
